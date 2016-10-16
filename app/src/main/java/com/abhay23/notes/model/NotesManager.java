76a437fcc24715d2e9f088f55732d14da2265e8d
abhay <abhay@cleartax.in>
2016-10-16 21:35:56 +0530

    Create a method in manager to return notes observable

--- app/src/main/java/com/abhay23/notes/model/NotesManager.java
+++ app/src/main/java/com/abhay23/notes/model/NotesManager.java
@@ -1,7 +1,17 @@
 package com.abhay23.notes.model;
 
+import java.util.List;
+import rx.Observable;
+
 public class NotesManager {
 
-  public NotesManager() {
+  private final Database database;
+
+  public NotesManager(Database database) {
+    this.database = database;
+  }
+
+  public Observable<List<Note>> getNotes() {
+    return Observable.fromCallable(database::getNotes);
   }
 }
