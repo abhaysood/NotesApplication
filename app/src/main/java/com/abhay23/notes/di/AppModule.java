985a3f063594f64f7f0b545a88b4df165b6ced97
abhay <abhay@cleartax.in>
2016-10-16 21:35:35 +0530

    Create a database class

--- app/src/main/java/com/abhay23/notes/di/AppModule.java
+++ app/src/main/java/com/abhay23/notes/di/AppModule.java
@@ -1,7 +1,11 @@
 package com.abhay23.notes.di;
 
 import com.abhay23.notes.NotesApplication;
+import com.abhay23.notes.model.Database;
+import com.abhay23.notes.model.NotesManager;
 import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
 
 @Module public class AppModule {
 
@@ -9,5 +13,17 @@ import dagger.Module;
 
   public AppModule(NotesApplication application) {
     this.application = application;
+  }
+
+  @Singleton
+  @Provides
+  public NotesManager provideNotesManager(Database database) {
+    return new NotesManager(database);
+  }
+
+  @Singleton
+  @Provides
+  public Database provideDatabase() {
+    return new Database();
   }
 }
---
+++ app/src/main/java/com/abhay23/notes/model/Database.java
@@ -0,0 +1,12 @@
+package com.abhay23.notes.model;
+
+import android.support.annotation.Nullable;
+import java.util.List;
+
+public class Database {
+
+  @Nullable
+  public List<Note> getNotes() {
+    return null;
+  }
+}
