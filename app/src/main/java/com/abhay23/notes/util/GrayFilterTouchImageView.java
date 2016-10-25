494e91449c38e020d39d8501cb925c46edbd9e11
abhay <soodabhay23@gmail.com>
2016-10-25 08:18:59 +0530

    Improve implementation of GrayScaleImageView touch listener

--- app/src/main/java/com/abhay23/notes/util/GrayFilterTouchImageView.java
+++ app/src/main/java/com/abhay23/notes/util/GrayFilterTouchImageView.java
@@ -3,6 +3,7 @@ package com.abhay23.notes.util;
 import android.content.Context;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 
 public class GrayFilterTouchImageView extends ImageView {
 
+  private Rect rect;
   private ColorMatrixColorFilter grayScaleFilter;
   private ColorMatrixColorFilter normalScaleFilter;
 
@@ -29,6 +31,7 @@ public class GrayFilterTouchImageView extends ImageView {
   }
 
   private void init() {
+    rect = new Rect();
     ColorMatrix grayScaleMatrix = new ColorMatrix();
     grayScaleMatrix.setSaturation(0);
     grayScaleFilter = new ColorMatrixColorFilter(grayScaleMatrix);
@@ -37,16 +40,34 @@ public class GrayFilterTouchImageView extends ImageView {
     grayScaleMatrix.setSaturation(1);
     normalScaleFilter = new ColorMatrixColorFilter(normalScaleMatrix);
 
-    setOnTouchListener(imageViewOnTouchListener);
+    handleTouch();
   }
 
-  View.OnTouchListener imageViewOnTouchListener = (v, event) -> {
-    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-      setColorFilter(grayScaleFilter);
-    } else if (event.getAction() == MotionEvent.ACTION_UP) {
-      setColorFilter(normalScaleFilter);
-    }
+  private void handleTouch() {
+    setOnTouchListener((view, event) -> {
+      switch (event.getAction()) {
 
-    return super.onTouchEvent(event);
-  };
+        case MotionEvent.ACTION_DOWN:
+          rect = new Rect(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+          GrayFilterTouchImageView.this.setColorFilter(grayScaleFilter);
+          break;
+
+        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_CANCEL:
+          GrayFilterTouchImageView.this.setColorFilter(normalScaleFilter);
+          break;
+
+        case MotionEvent.ACTION_MOVE:
+          if (!isTouchWithinBounds(view, event)) {
+            GrayFilterTouchImageView.this.setColorFilter(normalScaleFilter);
+          }
+          break;
+      }
+      return super.onTouchEvent(event);
+    });
+  }
+
+  private boolean isTouchWithinBounds(View view, MotionEvent event) {
+    return rect.contains(view.getLeft() + (int) event.getX(), view.getTop() + (int) event.getY());
+  }
 }
